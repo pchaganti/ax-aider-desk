21e9f6b9598d8e3bdc51f2a02ee2bb0a83eddb36
wladimiiir <wladimiiir@gmail.com>
2025-07-20 21:14:36 +0200

    feat: added temporary AIDER_DESK_OMIT_PYTHONUTF8 to omit PYTHONUTF8

--- src/main/project/project.ts
+++ src/main/project/project.ts
@@ -341,7 +341,7 @@ export class Project {
       ...process.env,
       ...environmentVariables,
       PYTHONPATH: AIDER_DESK_CONNECTOR_DIR,
-      PYTHONUTF8: '1',
+      PYTHONUTF8: process.env.AIDER_DESK_OMIT_PYTHONUTF8 ? undefined : '1',
       BASE_DIR: this.baseDir,
       CONNECTOR_SERVER_URL: `http://localhost:${SERVER_PORT}`,
     };
